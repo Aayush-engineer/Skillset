a40c51d52609db07e4ece7bca11259b716406e79
ayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2025-01-16 06:53:10 +0530

    feat(main): add NotificationsButton, NotFound, and Search components with search functionality and infinite scrolling

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -1,7 +1,10 @@
 import { validateRequest } from "@/auth";
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import streamServerClient from "@/lib/stream";
 import { Bookmark, Home } from "lucide-react";
 import Link from "next/link";
+import NotificationsButton from "./NotificationsButton";
 
 interface MenuBarProps {
   className?: string;
@@ -11,6 +14,16 @@ export default async function MenuBar({ className }: MenuBarProps) {
   const { user } = await validateRequest();
 
   if (!user) return null;
+
+  const [unreadNotificationsCount, unreadMessagesCount] = await Promise.all([
+    prisma.notification.count({
+      where: {
+        recipientId: user.id,
+        read: false,
+      },
+    }),
+    (await streamServerClient.getUnreadCount(user.id)).total_unread_count,
+  ]);
 
   return (
     <div className={className}>
@@ -25,6 +38,9 @@ export default async function MenuBar({ className }: MenuBarProps) {
           <span className="hidden lg:inline">Home</span>
         </Link>
       </Button>
+      <NotificationsButton
+        initialState={{ unreadCount: unreadNotificationsCount }}
+      />
       <Button
         variant="ghost"
         className="flex items-center justify-start gap-3"
